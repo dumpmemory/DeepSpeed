dumpmemory/DeepSpeed

commit 43125a753d68ab5a5fad6ddd56c1ff21d6e97d7f

Fix Evoformer compilation (#7760)
`EvoformerAttnBuilder` has some problems which preclude compiling the
extension on several scenarios (e.g., [isolated conda environment with
cuda toolchain](https://github.com/aqlaboratory/openfold-3/pull/34),
lack of hardware in the system) and breaks some standard DeepSpeed
configuration of target capabilities.

*Changes*

  - Fix evoformer CUTLASS detection:
- Allow to skip it, useful when CUTLASS is already correctly setup
(e.g., in a conda environment with CUTLASS and the CUDA toolchain)
- Fix misleading use of deprecated nvidia-cutlass pypi package by
actually using the provided bindings but discouraging this route as
[these bindings are not maintained
anymore](https://github.com/NVIDIA/cutlass/discussions/2119)

  - Fix evoformer compilation with no GPU is present:
- this is taken care correctly and more generally by
builder.compute_capability_args
    - allow for cross-compilation in systems without GPU
- allows for compilation against all available virtual architectures and
binary outputs
    - see e.g., https://github.com/deepspeedai/DeepSpeed/issues/5308

- Make all these changes configurable and explicit through documented
environment variables

Tested in all scenarios.

---------

Signed-off-by: Santi Villalba <sdvillal@gmail.com>
Co-authored-by: Masahiro Tanaka <81312776+tohtana@users.noreply.github.com>

op_builder/evoformer_attn.py
@@ -5,6 +5,7 @@
 
 from .builder import CUDAOpBuilder, installed_cuda_version
 import os
+from pathlib import Path
 
 
 class EvoformerAttnBuilder(CUDAOpBuilder):
@@ -14,31 +15,40 @@ class EvoformerAttnBuilder(CUDAOpBuilder):
     def __init__(self, name=None):
         name = self.NAME if name is None else name
         super().__init__(name=name)
-        self.cutlass_path = os.environ.get('CUTLASS_PATH')
+        self.cutlass_path = os.environ.get("CUTLASS_PATH")
+        # Target GPU architecture.
+        # Current useful values are: 70, 75, 80.
+        # For modern GPUs, 80 is the right value.
+        # No specializations of the kernel beyond Ampere are implemented
+        # See gemm_kernel_utils.h (also in cutlass example for fused attention) and cutlass/arch/arch.h
+        self.gpu_arch = os.environ.get("DS_EVOFORMER_GPU_ARCH")
 
     def absolute_name(self):
-        return f'deepspeed.ops.{self.NAME}_op'
+        return f"deepspeed.ops.{self.NAME}_op"
 
     def extra_ldflags(self):
         if not self.is_rocm_pytorch():
-            return ['-lcurand']
+            return ["-lcurand"]
         else:
             return []
 
     def sources(self):
-        src_dir = 'csrc/deepspeed4science/evoformer_attn'
-        return [f'{src_dir}/attention.cpp', f'{src_dir}/attention_back.cu', f'{src_dir}/attention_cu.cu']
+        src_dir = "csrc/deepspeed4science/evoformer_attn"
+        return [f"{src_dir}/attention.cpp", f"{src_dir}/attention_back.cu", f"{src_dir}/attention_cu.cu"]
 
     def nvcc_args(self):
         args = super().nvcc_args()
-        try:
-            import torch
-        except ImportError:
-            self.warning("Please install torch if trying to pre-compile kernels")
-            return args
-        major = torch.cuda.get_device_properties(0).major  #ignore-cuda
-        minor = torch.cuda.get_device_properties(0).minor  #ignore-cuda
-        args.append(f"-DGPU_ARCH={major}{minor}")
+        if not self.gpu_arch:
+            try:
+                import torch
+            except ImportError:
+                self.warning("Please install torch if trying to pre-compile kernels")
+                return args
+            major = torch.cuda.get_device_properties(0).major  #ignore-cuda
+            minor = torch.cuda.get_device_properties(0).minor  #ignore-cuda
+            args.append(f"-DGPU_ARCH={major}{minor}")
+        else:
+            args.append(f"-DGPU_ARCH={self.gpu_arch}")
         return args
 
     def is_compatible(self, verbose=False):
@@ -48,46 +58,64 @@ class EvoformerAttnBuilder(CUDAOpBuilder):
             if verbose:
                 self.warning("Please install torch if trying to pre-compile kernels")
             return False
+
         if self.cutlass_path is None:
             if verbose:
-                self.warning("Please specify the CUTLASS repo directory as environment variable $CUTLASS_PATH")
+                self.warning("Please specify CUTLASS location directory as environment variable CUTLASS_PATH")
+                self.warning(
+                    "Possible values are: a path, DS_IGNORE_CUTLASS_DETECTION and DS_USE_CUTLASS_PYTHON_BINDINGS")
             return False
-        if os.path.exists(f'{self.cutlass_path}/CHANGELOG.md'):
-            with open(f'{self.cutlass_path}/CHANGELOG.md', 'r') as f:
-                if '3.1.0' not in f.read():
-                    if verbose:
-                        self.warning("Please use CUTLASS version >= 3.1.0")
-                    return False
-        else:
-            # pip install nvidia-cutlass package
-            try:
-                import cutlass
-            except ImportError:
-                if verbose:
-                    self.warning("Please pip install nvidia-cutlass if trying to pre-compile kernels")
-                return False
-            cutlass_major, cutlass_minor = cutlass.__version__.split('.')[:2]
-            cutlass_compatible = (int(cutlass_major) >= 3 and int(cutlass_minor) >= 1)
-            if not cutlass_compatible:
-                if verbose:
-                    self.warning("Please use CUTLASS version >= 3.1.0")
-                return False
 
+        if self.cutlass_path != "DS_IGNORE_CUTLASS_DETECTION":
+            try:
+                self.include_paths()
+            except (RuntimeError, ImportError):
+                return False
+            # Check version in case it is a CUTLASS_PATH points to a CUTLASS checkout
+            if os.path.exists(f"{self.cutlass_path}/CHANGELOG.md"):
+                with open(f"{self.cutlass_path}/CHANGELOG.md", "r") as f:
+                    if "3.1.0" not in f.read():
+                        if verbose:
+                            self.warning("Please use CUTLASS version >= 3.1.0")
+                        return False
+
+        # Check CUDA and GPU capabilities
         cuda_okay = True
-        if not self.is_rocm_pytorch() and torch.cuda.is_available():  #ignore-cuda
-            sys_cuda_major, _ = installed_cuda_version()
-            torch_cuda_major = int(torch.version.cuda.split('.')[0])
-            cuda_capability = torch.cuda.get_device_properties(0).major  #ignore-cuda
-            if cuda_capability < 7:
-                if verbose:
-                    self.warning("Please use a GPU with compute capability >= 7.0")
-                cuda_okay = False
-            if torch_cuda_major < 11 or sys_cuda_major < 11:
-                if verbose:
-                    self.warning("Please use CUDA 11+")
-                cuda_okay = False
+        if not os.environ.get("DS_IGNORE_CUDA_DETECTION"):
+            if not self.is_rocm_pytorch() and torch.cuda.is_available():  #ignore-cuda
+                sys_cuda_major, _ = installed_cuda_version()
+                torch_cuda_major = int(torch.version.cuda.split(".")[0])
+                cuda_capability = torch.cuda.get_device_properties(0).major  #ignore-cuda
+                if cuda_capability < 7:
+                    if verbose:
+                        self.warning("Please use a GPU with compute capability >= 7.0")
+                    cuda_okay = False
+                if torch_cuda_major < 11 or sys_cuda_major < 11:
+                    if verbose:
+                        self.warning("Please use CUDA 11+")
+                    cuda_okay = False
         return super().is_compatible(verbose) and cuda_okay
 
     def include_paths(self):
-        includes = [f'{self.cutlass_path}/include', f'{self.cutlass_path}/tools/util/include']
-        return includes
+        # Assume the user knows best and CUTLASS location is already setup externally
+        if self.cutlass_path == "DS_IGNORE_CUTLASS_DETECTION":
+            return []
+        # Use header files vendored with deprecated python packages
+        if self.cutlass_path == "DS_USE_CUTLASS_PYTHON_BINDINGS":
+            try:
+                import cutlass_library
+                cutlass_path = Path(cutlass_library.__file__).parent / "source"
+            except ImportError:
+                self.warning("Please pip install nvidia-cutlass (note that this is deprecated and likely outdated)")
+                raise
+        # Use hardcoded path in CUTLASS_PATH
+        else:
+            cutlass_path = Path(self.cutlass_path)
+        cutlass_path = cutlass_path.resolve()
+        if not cutlass_path.is_dir():
+            raise RuntimeError(f"CUTLASS_PATH {cutlass_path} does not exist")
+        include_dirs = cutlass_path / "include", cutlass_path / "tools" / "util" / "include"
+        include_dirs = [include_dir for include_dir in include_dirs if include_dir.is_dir()]
+        if not include_dirs:
+            raise RuntimeError(f"CUTLASS_PATH {cutlass_path} does not contain any include directories")
+        return include_dirs

commit c9e652cfae616254b287a02851593dad7edee8da

Fix compilation of Evoformer (#7862)
`EvoformerAttnBuilder` returns instances of `Path` from `include_paths`
which then cause failures in `OpBuilder.builder` when passing them to
`strip_empty_entries` that calls `len` on them which isn't defined for
`Path` instances:
>   TypeError: object of type 'PosixPath' has no len()

Fixes regression introduced in #7760

cc @sdvillal

Signed-off-by: Alexander Grund <alexander.grund@tu-dresden.de>

op_builder/evoformer_attn.py
@@ -115,7 +115,7 @@ class EvoformerAttnBuilder(CUDAOpBuilder):
         if not cutlass_path.is_dir():
             raise RuntimeError(f"CUTLASS_PATH {cutlass_path} does not exist")
         include_dirs = cutlass_path / "include", cutlass_path / "tools" / "util" / "include"
-        include_dirs = [include_dir for include_dir in include_dirs if include_dir.is_dir()]
+        include_dirs = [str(include_dir) for include_dir in include_dirs if include_dir.is_dir()]
         if not include_dirs:
             raise RuntimeError(f"CUTLASS_PATH {cutlass_path} does not contain any include directories")
         return include_dirs